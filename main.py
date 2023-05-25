d2f005f942567c711acef4c8fdbf30d66be6eb06
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-05-25 11:43:32 +0300

    versio 2 lisätty käyttöliittymään ja se osaa luoda neuroverkon painomatriisit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import versio2.draft2
 
 x = int(input("mitä ajetaan?"))
 if x == 1:
     import versio1.draft1
+if x == 2:
+    LR = float(input("Anna learning rate: "))
+    ML = int(input("Montako keskikerrosta: "))
+    versio2.draft2.LM_model(LR,ML)
--- versio2/draft2.py
+++ versio2/draft2.py
@@ -1,3 +1,20 @@
 import pandas
 import numpy as np
+import aktivaatiofunktiot.activation_functions as af
 
+class LM_model:
+    #Luodaan neuroverkko. Annetaan parametreja ja tallennetaan piilotetut kerrokset listaan yhteensopivina matriiseina matriiseina
+    def __init__(self, learning_rate, number_of_middle_layers):
+        self.LR = learning_rate
+        self.I_dim = int(input("Syötteen dimensio: "))
+        self.middle_layers = []
+        for i in range(number_of_middle_layers):
+            dim = int(input("Seuraavan kerroksen dimensio: "))
+            if i == 0:
+                self.middle_layers.append(np.random.uniform(-1, 1, (self.I_dim, dim)))
+                prev_dim = dim
+            else:
+                self.middle_layers.append(np.random.uniform(-1, 1, (prev_dim, dim)))
+                prev_dim = dim
+        self.O_dim = int(input("Tulosteen dimensio: "))
+        self.middle_to_output = np.random.uniform(-1, 1, (prev_dim, self.O_dim))

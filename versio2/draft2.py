62fecd6d7052fe55e3a5a28e6b9700f7de92f2cd
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-05-25 13:17:06 +0300

    verkon luonti onnistuu, verkon läpi voi ajaa syötteen

--- versio2/draft2.py
+++ versio2/draft2.py
@@ -21,3 +21,30 @@ class LM_model:
         self.O_dim = int(input("Tulosteen dimensio: "))
         self.middle_to_output_weight_matrix = np.random.uniform(-1, 1, (self.O_dim,prev_dim))
         self.middle_to_output_bias = np.random.uniform(-1, 1, (self.O_dim,1))
+
+    def get_data(self, name_of_file):
+        datatype = input("Is the data training or validation data? [Y/n]: ")
+        self.data =pandas.read_excel(name_of_file, engine="odf")
+        if datatype == "Y":
+            self.output_comparison = self.data.output
+            self.data = self.data.drop(['output'],axis=1)
+        self.data = np.asarray(self.data)
+        self.data_count = len(self.data)
+    
+    def forward_propagation(self, input):
+        for layer in range(len(self.middle_layer_weight_matrices)):
+            if layer == 0:
+                preActivation = np.matmul(self.middle_layer_weight_matrices[layer],input)+self.middle_layer_biases[layer]
+                postActivation = []
+                for i in preActivation:
+                    postActivation.append(af.elu(np.sum(i)))
+            else:
+                preActivation = np.matmul(self.middle_layer_weight_matrices[layer],postActivation)+self.middle_layer_biases[layer]
+                postActivation = []
+                for i in preActivation:
+                    postActivation.append(af.elu(np.sum(i)))
+        preActivation = np.matmul(self.middle_to_output_weight_matrix, postActivation)+self.middle_to_output_bias
+        postActivation = []
+        for i in preActivation:
+            postActivation.append(af.elu(np.sum(i)))
+        return postActivation

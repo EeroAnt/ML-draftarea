5e2697e007f48f7512f381965f10cf9068dcb9af
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-05-26 14:41:36 +0300

    se toimii. se on ruma, mut se toimii

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import versio2.draft2
 
-x = int(input("mitä ajetaan?"))
+x = 2#int(input("mitä ajetaan?"))
 if x == 1:
     #tämä on paskaa
     import versio1.draft1
 if x == 2:
-    LR = float(input("Anna learning rate: "))
-    ML = int(input("Montako keskikerrosta: "))
+    LR = 0.3#float(input("Anna learning rate: "))
+    ML = 3#int(input("Montako keskikerrosta: "))
     malli = versio2.draft2.LM_model(LR,ML)
     print(malli.input([1,1,1]))
     print(malli.loss_function([1,1,1],[2,2]))
+    for i in range(10):
+        malli.training()
+        print(malli.loss_function([1,1,1],[2,2]))
--- versio2/draft2.py
+++ versio2/draft2.py
@@ -6,7 +6,7 @@ class LM_model:
     #Luodaan neuroverkko. Annetaan parametreja ja tallennetaan piilotetut kerrokset listaan yhteensopivina matriisei
     def __init__(self, learning_rate, number_of_weight_matrices):
         self.LR = learning_rate
-        self.I_dim = int(input("Syötteen dimensio: "))
+        self.I_dim = 3#int(input("Syötteen dimensio: "))
         self.weight_matrices = []
         self.biases = []
         for i in range(number_of_weight_matrices):
@@ -18,7 +18,7 @@ class LM_model:
                 self.weight_matrices.append(np.random.uniform(-1, 1, (dim, prev_dim)))
                 prev_dim = dim
             self.biases.append(np.random.uniform(-1,1, (dim,1)))
-        self.O_dim = int(input("Tulosteen dimensio: "))
+        self.O_dim = 2#int(input("Tulosteen dimensio: "))
         self.weight_matrices.append(np.random.uniform(-1, 1, (self.O_dim,prev_dim)))
         self.biases.append(np.random.uniform(-1, 1, (self.O_dim,1)))
         self.activation_function_selection()
@@ -31,7 +31,7 @@ class LM_model:
         print("3: LReLU")
         print("4: ELU")
         print("5: arctan(x) + x")
-        self.selection = int(input("Mikä funktio saisi olla? "))
+        self.selection = 1#int(input("Mikä funktio saisi olla? "))
     
     def activation_function(self,x):
         if self.selection == 1:     
@@ -66,20 +66,26 @@ class LM_model:
             self.data = self.data.drop(['output'],axis=1)
         self.data = np.asarray(self.data)
         self.data_count = len(self.data)
+    
     #syöte läpi verkosta
     def input(self, input):
+        self.postActivations_temp = []
         for layer in range(len(self.weight_matrices)):
             if layer == 0:
                 preActivation = np.matmul(self.weight_matrices[layer],input)+self.biases[layer]
                 postActivation = []
                 for i in preActivation:
                     postActivation.append(self.activation_function(np.sum(i)))
+                self.postActivations_temp.append(postActivation)
             else:
                 preActivation = np.matmul(self.weight_matrices[layer],postActivation)+self.biases[layer]
                 postActivation = []
                 for i in preActivation:
                     postActivation.append(self.activation_function(np.sum(i)))
+                self.postActivations_temp.append(postActivation)
+        self.postActivations_temp.reverse()
         return postActivation
+    
     #least-squares-loss-funktio:
     def loss_function(self, input, expected_output):
         output = self.input(input)
@@ -87,3 +93,57 @@ class LM_model:
         for i in range(len(output)):
             loss.append(output[i]-expected_output[i])
         return 0.5*np.linalg.norm(loss)**2
+    
+    def activation_func_derivs(self):
+        activations_function_derivatives = []
+        for vector in self.postActivations_temp:
+            derivative = np.zeros((len(vector),len(vector)))
+            for i in range(len(vector)):
+                derivative[i][i]=self.activation_function_deriv(vector[i])
+            activations_function_derivatives.append(derivative)
+        return activations_function_derivatives
+
+    def loss_function_deriv(self,expected_output):
+        loss_function_deriv = []
+        for i in range(len(self.postActivations_temp[0])):
+            loss_function_deriv.append(self.postActivations_temp[0][i]-expected_output[i])
+        return loss_function_deriv
+
+    def derivation(self,sampledata,expected_output):
+        self.matrix_derivatives = []
+        self.bias_derivatives = []
+        act_fnc_ds = self.activation_func_derivs()
+        n = len(self.weight_matrices)
+        for i in range(n):
+            if i == 0:
+                self.bias_derivatives.append(np.matmul(self.loss_function_deriv(expected_output),act_fnc_ds[i]))
+                self.matrix_derivatives.append(np.matmul(self.postActivations_temp[i],self.bias_derivatives[i]))
+            elif i < n-1:
+                self.bias_derivatives.append(np.matmul(self.bias_derivatives[i-1],np.matmul(self.weight_matrices[n-i],act_fnc_ds[i])))
+                self.matrix_derivatives.append(np.matmul(self.postActivations_temp[i],self.bias_derivatives[i]))
+            else:
+                self.bias_derivatives.append(np.matmul(self.bias_derivatives[i-1],np.matmul(self.weight_matrices[n-i],act_fnc_ds[i])))
+                self.matrix_derivatives.append(np.matmul(sampledata,self.bias_derivatives[i]))
+        self.matrix_derivatives.reverse()
+        self.bias_derivatives.reverse()
+
+    def training(self):
+        data = [[1,1,1]]#self.get_data('randomtestdata.ods')
+        expected_output = [[2,2]]
+        matrix_corrections = []
+        bias_corrections = []
+        counter = 0
+        n = len(self.weight_matrices)
+        for i in range(n):
+            matrix_corrections.append(np.zeros((len(self.weight_matrices[i]),len(self.weight_matrices[i][0]))))
+            bias_corrections.append(np.zeros(len(self.biases[i])))
+        for sample in range(len(data)):
+            counter += 1
+            self.input(data[sample])
+            self.derivation(data[sample],expected_output[sample])
+            for j in range(n):
+                matrix_corrections[j] = np.add(matrix_corrections[j], self.matrix_derivatives[j])
+                bias_corrections[j] = np.add(bias_corrections[j], self.bias_derivatives[j])
+        for i in range(n):
+            self.weight_matrices[i] = np.add(self.weight_matrices[i], -1*(self.LR/counter) * matrix_corrections[i])
+            self.biases[i] = np.add(self.biases[i], (-1*self.LR/counter) * bias_corrections[i])

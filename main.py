1192bf714cd971ee12f6c0b0cafa371457e3c977
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-05-29 12:00:31 +0300

    nää vielä ylös

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ if x == 1:
     #tämä on paskaa
     import versio1.draft1
 if x == 2:
-    LR = 0.3#float(input("Anna learning rate: "))
+    LR = 0.7#float(input("Anna learning rate: "))
     ML = 3#int(input("Montako keskikerrosta: "))
     malli = versio2.draft2.LM_model(LR,ML)
-    print(malli.input([1,1,1]))
-    print(malli.loss_function([1,1,1],[2,2]))
-    for i in range(10):
+    for i in range(1000):
         malli.training()
-        print(malli.loss_function([1,1,1],[2,2]))
+        if i%100 == 0:
+            print(malli.validation())

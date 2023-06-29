988497cc7caa17a1552b28fbc257a1df3e51a223
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-06-29 11:10:27 +0300

    tää skripti liittää vuosikatteiden ekseleitä eri vuosilta ja yhdistää ne kuntien pohjalta

---
+++ vuosikateekselinpyorittaja.py
@@ -0,0 +1,27 @@
+import pandas
+import sqlite3
+import os
+
+dataframe = pandas.read_excel(os.path.realpath("talousdata/katteet2008.xlsx"))
+db = sqlite3.connect("vuosikate.db")
+db.isolation_level = None
+
+#aja ensiksi vain nämä. Tarkista taulukon koko ja rivien rakenne
+print(dataframe.loc[2].to_numpy())
+print(dataframe.info())
+
+kunta = -1
+for i in range(2,417):
+    data = dataframe.loc[i].to_numpy()
+    if data[0]<10:
+        kunta ="`00"+str(int(data[0]))+"`"
+    elif data[0]<100:
+        kunta ="`0"+str(int(data[0]))+"`"
+    else:
+        kunta ="`"+str(int(data[0]))+"`"
+    db.execute(
+               f"CREATE TABLE if not exists {kunta} (id INTEGER PRIMARY KEY, vuosi INTEGER,"+
+               "toimintakate, vuosikate)")
+    db.execute(
+        f"INSERT INTO {kunta}(vuosi, toimintakate, vuosikate) values"+
+        "(?,?,?);", dataframe.loc[i][1:])

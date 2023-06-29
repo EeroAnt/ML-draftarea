71406efb9054c7a633d97fe68771d7e024af8013
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-06-29 11:31:01 +0300

    vuosikate-tietokanta valmis vuosilta 2002-2014

--- vuosikateekselinpyorittaja.py
+++ vuosikateekselinpyorittaja.py
@@ -2,7 +2,7 @@ import pandas
 import sqlite3
 import os
 
-dataframe = pandas.read_excel(os.path.realpath("talousdata/katteet2008.xlsx"))
+dataframe = pandas.read_excel(os.path.realpath("talousdata/katteet2002_2006.xlsx"))
 db = sqlite3.connect("vuosikate.db")
 db.isolation_level = None
 
@@ -10,18 +10,20 @@ db.isolation_level = None
 print(dataframe.loc[2].to_numpy())
 print(dataframe.info())
 
-kunta = -1
-for i in range(2,417):
+# seuraava pätkä yhdistää uuden excelin datan olemassaolevaan tietokantaan.
+
+for i in range(2,2247):
     data = dataframe.loc[i].to_numpy()
-    if data[0]<10:
-        kunta ="`00"+str(int(data[0]))+"`"
-    elif data[0]<100:
-        kunta ="`0"+str(int(data[0]))+"`"
-    else:
-        kunta ="`"+str(int(data[0]))+"`"
-    db.execute(
-               f"CREATE TABLE if not exists {kunta} (id INTEGER PRIMARY KEY, vuosi INTEGER,"+
-               "toimintakate, vuosikate)")
+    if data[0]>0:
+        if data[0]<10:
+            kunta ="`00"+str(int(data[0]))+"`"
+        elif data[0]<100:
+            kunta ="`0"+str(int(data[0]))+"`"
+        else:
+            kunta ="`"+str(int(data[0]))+"`"
+        db.execute(
+                f"CREATE TABLE if not exists {kunta} (id INTEGER PRIMARY KEY, vuosi INTEGER,"+
+                "toimintakate, vuosikate)")
     db.execute(
         f"INSERT INTO {kunta}(vuosi, toimintakate, vuosikate) values"+
         "(?,?,?);", dataframe.loc[i][1:])

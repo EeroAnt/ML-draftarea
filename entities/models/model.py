9a49c86941b792d679b54f38b8407f4365af54e2
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-10-25 09:18:57 +0300

    toimiva malli online

--- entities/models/model.py
+++ entities/models/model.py
@@ -1,26 +1,48 @@
+import os
 from keras import layers
 from keras import models
-import pandas
 import numpy as np
+import sqlite3
+import random
 
-def get_data(name_of_file):
-        data = pandas.read_excel(name_of_file, engine="odf")
-        output = data.output1
-        data = data.drop(['output1'],axis=1)
-        data = np.asarray(data)
-        return (data, output)
+def get_data():
+        database = sqlite3.connect("masterdb.db")
+        data= []
+        output_data = []
+        input_data = []
+        for i in range(1000):
+            if i <10:
+                number = f"00{i}"
+            elif i <100:
+                number = f"0{i}"
+            else:
+                number = str(i)
+            if database.execute(f"SELECT * FROM sqlite_master WHERE name ='vaestorakenne{number}' and type='table';").fetchall() != []:
+                rows_to_add = database.execute(f"SELECT * From vaestorakenne{number} as A LEFT JOIN vuosikate{number} as B ON "+
+                                               "A.vuosi = B.vuosi where A.id is not NULL and B.id is not null and b.vuosikate is not NULL;").fetchall()
+                data = data+rows_to_add
+        random.shuffle(data)
+        for i in data:
+            output_data.append(i[-1])
+            input_data.append(i[:-2])
+        dt=np.dtype('int')
+        input_data = np.asarray(input_data,dtype=dt)
+        return (input_data, output_data)
 
 def normalize(data):
     mean = data.mean(axis=0)
     data = data - mean
     std = data.std(axis=0)
+    np.seterr(divide='ignore', invalid='ignore')
     data = data / std
     return data
 
-(train_data, train_targets) = get_data('randomtestdata.ods')
-data = normalize(train_data)
+def prep_data():
+    (train_data, train_targets) = get_data()
+    data = normalize(train_data)
+    return data, train_targets
 
-def build_model():
+def build_model(train_data):
     model = models.Sequential()
     model.add(layers.Dense(64, activation='relu',
                            input_shape=(train_data.shape[1],)))
--- main.py
+++ main.py
@@ -1,3 +1,96 @@
 import entities.models.model as m
+import numpy as np
 
-model = m.build_model()
+# model = m.build_model()
+(train_data,train_targets) = m.prep_data()
+
+k = 4
+num_val_samples = len(train_data) // 4
+num_epochs = 100
+all_scores = []
+
+# for i in range(k):
+#     print('prosessing fold #', i)
+#     val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+#     val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+    # val_data = np.array(val_data)
+    # val_targets = np.array(val_targets)
+
+#     partial_train_data = np.concatenate(
+#         [train_data[:i * num_val_samples],
+#          train_data[(i + 1) * num_val_samples:]],
+#         axis=0)
+#     partial_train_targets = np.concatenate(
+#         [train_targets[:i * num_val_samples],
+#          train_targets[(i + 1) * num_val_samples:]],
+#         axis=0)
+    
+#     model = m.build_model(train_data)
+#     model.fit(partial_train_data, partial_train_targets,
+#               epochs=num_epochs, batch_size=1, verbose=0)
+#     val_mse, val_mae = model.evaluate(val_data,val_targets,verbose=0)
+#     all_scores.append(val_mae)
+
+# print(all_scores)
+# print(np.mean(all_scores))
+
+
+# tutkitaan epochien määrän merkitystä ja etsitään overfittauksen alkamista:
+
+num_epochs = 500
+all_mae_histories = []
+
+for i in range(k):
+    print('prosessing fold #', i)
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+    val_data = np.array(val_data)
+    val_targets = np.array(val_targets)
+
+    partial_train_data = np.concatenate(
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+        axis=0)
+    partial_train_targets = np.concatenate(
+        [train_targets[:i * num_val_samples],
+         train_targets[(i + 1) * num_val_samples:]],
+        axis=0)
+    
+    model = m.build_model(train_data)
+    history = model.fit(partial_train_data, partial_train_targets,
+                        validation_data=(val_data, val_targets),
+                        epochs=num_epochs, batch_size=1, verbose=0)
+    mae_history = history.history['val_mae']
+    all_mae_histories.append(mae_history)
+
+# jokaisen epochin keskiarvot
+average_mae_history = [
+    np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+
+# plottausta
+
+import matplotlib.pyplot as plt
+
+plt.plot(range(1, len(average_mae_history) + 1), average_mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()
+
+# plotti on haastavaa luettavaa. Poistetaan ensimmäiset 10 datapistettä ja smoothataan
+
+def smooth_curve(points, factor=0.9):
+    smoothed_points=[]
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous*factor+ point*(1-factor))
+        else:
+            smoothed_points.append(point)
+    return smoothed_points
+
+smooth_mae_history = smooth_curve(average_mae_history[10:])
+
+plt.plot(range(1, len(smooth_mae_history) + 1), smooth_mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()

05de0d6f76d5a118e21ea18f469281e8079f3505
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-01-02 15:00:28 +0200

    renaming stuff

--- excel_into_db.py
+++
@@ -1,5 +0,0 @@
-from apupalikoita import vuosikateekselinpyorittaja as op
-
-op.add_to_db(op.read_excel("Alueelle_pendeloivat"))
-op.remove_tables()
-# op.read_excel("Alueelle_pendeloivat")
---
+++ run_db_stuff.py
@@ -0,0 +1,6 @@
+from apupalikoita import vuosikateekselinpyorittaja as op
+
+# op.add_to_db(op.read_excel("Alueelle_pendeloivat"))
+# op.remove_tables()
+# op.read_excel("Alueelle_pendeloivat")
+op.add_table("Vuosikate")

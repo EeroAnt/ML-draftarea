f33e30623b9629639e0885d316697c66551e7d19
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-01-02 20:28:09 +0200

    väkiluvut lisätty omaksi taulukseen kokoluokkiin jakoa auttamaan

--- apupalikoita/vuosikateekselinpyorittaja.py
+++ apupalikoita/vuosikateekselinpyorittaja.py
@@ -6,56 +6,63 @@ import numpy as np
 ## Täällä kun kahtoo parametrit kohdilleen, niin saa lisättyä excel_into_dp.py ajamalla asioita master-tietokantaan. Ei pidä ajaa turhaan, tulee harmeja.
 
 def read_excel(name):
-	dataframe = pandas.read_excel(os.path.realpath(f"talousdata/{name}.xlsx"))
+	dataframe = pandas.read_excel(os.path.realpath(f"exceleita/{name}.xlsx"))
 	# näillä voi tutkia taulukon rakennetta
 	# print(dataframe.loc[2].to_numpy())
 	# print(dataframe.info())
-	# print(dataframe[5:-35])
-	return dataframe[5:-35]
+	# print(dataframe[2:-39])
+	return dataframe[2:-39]
 
 def insert_from_excel(dataframe):
 	db = sqlite3.connect("masterdb.db")
 	db.isolation_level = None
 	kuntakoodit = municipality_codes()
-	for i in range(5,10820):
+	for i in range(2,311):
 		data = dataframe.loc[i].to_numpy()
-		if type(data[0]) == str:
-			kunta = kuntakoodit[data[0]]
-		sql_query_insert = tuple(data[1:])+tuple([kunta])
-		sql=(f"INSERT INTO Pendeloivat ("+
-				"vuosi,"+
-				"Toinen24,"+
-				"Toinen34,"+
-				"Toinen44,"+
-				"Toinen54,"+
-				"Toinen64,"+
-				"Toinen74,"+
-				"Alin24,"+
-				"Alin34,"+
-				"Alin44,"+
-				"Alin54,"+
-				"Alin64,"+
-				"Aline74,"+
-				"Ylempi24,"+
-				"Ylempi34,"+
-				"Ylempi44,"+
-				"Ylempi54,"+
-				"Ylempi64,"+
-				"Ylempi74,"+
-				"Ei24,"+
-				"Ei34,"+
-				"Ei44,"+
-				"Ei54,"+
-				"Ei64,"+
-				"Ei74,"+
-				"kunta_id)"+
-				"values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);")
-		db.execute(sql, sql_query_insert)
+		sql = ("INSERT INTO Asukasluku ("+
+		 		"kunta_id, "+
+				"Asukasluvun_ka) "+
+				"values (?,?);"
+				)
+		db.execute(sql,(kuntakoodit[data[0]],int(data[-1])))
 		db.commit()
+		# if type(data[0]) == str:
+		# 	kunta = kuntakoodit[data[0]]
+		# sql_query_insert = tuple(data[1:])+tuple([kunta])
+		# sql=(f"INSERT INTO Pendeloivat ("+
+		# 		"vuosi,"+
+		# 		"Toinen24,"+
+		# 		"Toinen34,"+
+		# 		"Toinen44,"+
+		# 		"Toinen54,"+
+		# 		"Toinen64,"+
+		# 		"Toinen74,"+
+		# 		"Alin24,"+
+		# 		"Alin34,"+
+		# 		"Alin44,"+
+		# 		"Alin54,"+
+		# 		"Alin64,"+
+		# 		"Aline74,"+
+		# 		"Ylempi24,"+
+		# 		"Ylempi34,"+
+		# 		"Ylempi44,"+
+		# 		"Ylempi54,"+
+		# 		"Ylempi64,"+
+		# 		"Ylempi74,"+
+		# 		"Ei24,"+
+		# 		"Ei34,"+
+		# 		"Ei44,"+
+		# 		"Ei54,"+
+		# 		"Ei64,"+
+		# 		"Ei74,"+
+		# 		"kunta_id) "+
+		# 		"values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);")
+		# db.execute(sql, sql_query_insert)
+		# db.commit()
 
 def municipality_codes():
 	kuntakoodit = {}
-	with open("talousdata/kuntatunnukset.csv", mode='r',encoding='cp1250',newline='') as csv_file:
+	with open("exceleita/kuntatunnukset.csv", mode='r',encoding='cp1250',newline='') as csv_file:
 		csv_read = csv.DictReader(csv_file,delimiter=";")
 		for row in csv_read:
 			if row["classificationItemName"]!='Vĺrdö':
@@ -82,25 +89,8 @@ def add_table(name):
 	db.isolation_level = None
 	sql =(f"CREATE Table if not exists {name} ("+
 			"id INTEGER PRIMARY KEY, "+
-			"vuosi, "+
-			"Nuoria_yhteensa, "+
-			"Tyovoima_nuoret, "+
-			"Tyolliset_nuoret, "+
-			"Alle_15, "+
-			"Opiskelijat_nuoret, "+
-			"Elake_nuoret, "+
-			"Aikuisia_yhteensa, "+
-			"Tyovoima_aikuiset, "+
-			"Tyolliset_aikuiset, "+
-			"Opiskelijat_aikuiset, "+
-			"Varusmiehet_aikuiset, "+
-			"Elake_aikuiset, "+
-			"Iakkaat_yhteensa, "+
-			"Tyovoima_iakkaat, "+
-			"Tyolliset_iakkaat, "+
-			"Opiskelijat_iakkaat, "+
-			"Elake_iakkaat, "+
-			"Kunta_id);")
+			"kunta_id, "+
+			"Asukasluvun_ka);")
 	db.execute(sql)
 
 def null_mapper(x):
--- run_db_stuff.py
+++ run_db_stuff.py
@@ -1,7 +1,7 @@
 from apupalikoita import vuosikateekselinpyorittaja as op
 
-# op.add_to_db(op.read_excel("Alueelle_pendeloivat"))
-op.remove_numbered_tables()
-# op.read_excel("Alueelle_pendeloivat")
-# op.add_table("Vaestorakenne")
+# op.remove_numbered_tables()
+# op.read_excel("Vakilukuja")
+# op.add_table("Asukasluku")
 # op.insert_from_numbered_table()
+op.insert_from_excel(op.read_excel("Vakilukuja"))

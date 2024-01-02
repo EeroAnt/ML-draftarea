9b335fc72807d036fc60accf6c3d84d44102b84a
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-01-02 14:33:57 +0200

    spaceja tabeihin

--- entities/models/model.py
+++ entities/models/model.py
@@ -2,11 +2,11 @@ from keras import layers
 from keras import models
 
 def build_model(train_data):
-    model = models.Sequential()
-    model.add(layers.Dense(120, activation='relu',
-                           input_shape=(train_data.shape[1],)))
-    model.add(layers.Dense(120, activation='relu'))
-    model.add(layers.Dense(120, activation='relu'))
-    model.add(layers.Dense(1))
-    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
-    return model
+	model = models.Sequential()
+	model.add(layers.Dense(120, activation='relu',
+			  input_shape=(train_data.shape[1],)))
+	model.add(layers.Dense(120, activation='relu'))
+	model.add(layers.Dense(120, activation='relu'))
+	model.add(layers.Dense(1))
+	model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+	return model

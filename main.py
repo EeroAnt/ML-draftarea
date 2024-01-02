69aa13e255e46beb2a3fc79a795cb75bb86df3a6
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-01-02 14:31:04 +0200

    vaihdetaan spaceja tabeihin

--- main.py
+++ main.py
@@ -13,27 +13,27 @@ num_val_samples = len(train_data) // 4
 num_epochs = 100
 all_scores = []
 
-# for i in range(k):
-#     print('prosessing fold #', i)
-#     val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
-#     val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
-    # val_data = np.array(val_data)
-    # val_targets = np.array(val_targets)
+for i in range(k):
+	print('prosessing fold #', i)
+	val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+	val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+	val_data = np.array(val_data)
+	val_targets = np.array(val_targets)
 
-#     partial_train_data = np.concatenate(
-#         [train_data[:i * num_val_samples],
-#          train_data[(i + 1) * num_val_samples:]],
-#         axis=0)
-#     partial_train_targets = np.concatenate(
-#         [train_targets[:i * num_val_samples],
-#          train_targets[(i + 1) * num_val_samples:]],
-#         axis=0)
-    
-#     model = m.build_model(train_data)
-#     model.fit(partial_train_data, partial_train_targets,
-#               epochs=num_epochs, batch_size=1, verbose=0)
-#     val_mse, val_mae = model.evaluate(val_data,val_targets,verbose=0)
-#     all_scores.append(val_mae)
+	partial_train_data = np.concatenate(
+		[train_data[:i * num_val_samples],
+		train_data[(i + 1) * num_val_samples:]],
+		axis=0)
+	partial_train_targets = np.concatenate(
+		[train_targets[:i * num_val_samples],
+		train_targets[(i + 1) * num_val_samples:]],
+		axis=0)
+
+	model = m.build_model(train_data)
+	model.fit(partial_train_data, partial_train_targets,
+			 epochs=num_epochs, batch_size=1, verbose=0)
+	val_mse, val_mae = model.evaluate(val_data,val_targets,verbose=0)
+	all_scores.append(val_mae)
 
 # print(all_scores)
 # print(np.mean(all_scores))
@@ -69,7 +69,7 @@ for i in range(k):
 
 # jokaisen epochin keskiarvot
 average_mae_history = [
-    np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+	np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
 
 # plottausta
 
@@ -89,14 +89,14 @@ plt.show()
 # plotti on haastavaa luettavaa. Poistetaan ensimmäiset 10 datapistettä ja smoothataan
 
 def smooth_curve(points, factor=0.9):
-    smoothed_points=[]
-    for point in points:
-        if smoothed_points:
-            previous = smoothed_points[-1]
-            smoothed_points.append(previous*factor+ point*(1-factor))
-        else:
-            smoothed_points.append(point)
-    return smoothed_points
+	smoothed_points=[]
+	for point in points:
+		if smoothed_points:
+			previous = smoothed_points[-1]
+			smoothed_points.append(previous*factor+ point*(1-factor))
+		else:
+			smoothed_points.append(point)
+	return smoothed_points
 
 smooth_mae_history = smooth_curve(average_mae_history[10:])
 

649120faf18a46b2c5941e9c46a05677136fb13a
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-01-10 14:23:44 +0200

    verkon rakenne siirretty muutujaksi ylimpään funktioon, jotta mahd paljon valintoja voisi tehdä yhdessä paikkaa

--- entities/models/model.py
+++ entities/models/model.py
@@ -1,12 +1,12 @@
 from keras import layers
 from keras import models
 
-def build_model(train_data):
+def build_model(train_data, network_structure):
 	model = models.Sequential()
-	model.add(layers.Dense(120, activation='relu',
+	model.add(layers.Dense(network_structure[0][0], activation=network_structure[0][1],
 			  input_shape=(train_data.shape[1],)))
-	model.add(layers.Dense(120, activation='relu'))
-	model.add(layers.Dense(120, activation='relu'))
-	model.add(layers.Dense(3))
+	for i in range(1,len(network_structure)-1):
+		model.add(layers.Dense(network_structure[i][0], activation=network_structure[i][1]))
+	model.add(layers.Dense(network_structure[-1]))
 	model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'], run_eagerly=True)
 	return model
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from entities.tests import test_with_plots
+from services.training import test_with_plots
+
+network_structure = [
+	(120, 'relu'),
+	(120, 'relu'),
+	(120, 'relu'),
+	3
+]
 
 
-test_with_plots(1000, 4)
-
-
+test_with_plots(num_epochs=3, k=4, network_structure=network_structure)
